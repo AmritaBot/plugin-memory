01fa39b9c3dfb09741fee1592a42d39e74a80d1e
John Richard <67693593+JohnRichard4096@users.noreply.github.com>
2026-02-11 15:20:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# plugin-template
+
+Amrita的插件模板
+
+## How to start
+
+1. 使用`uv venv`初始化开发环境
+2. 使用`uv sync`同步虚拟环境
+3. 开始你的开发吧。
+
+## Environment
+
+请在Actions配置`PYPI_API_TOKEN`来使用自动发布脚本。
+
+## 开发规范
+
+插件名称请使用`amrita-plugin-`开头。
+
+插件名称请使用`-`连接单词。
---
+++ amrita_plugin_name/config.py
@@ -0,0 +1,21 @@
+# Configuration for your_plugin_name plugin
+from amrita.config_manager import BaseDataManager
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+
+class ConfigFile(BaseModel):
+    ...
+    # Add your file configuration here
+
+class DataManager(BaseDataManager):
+    # This is a sample data manager
+    config: ConfigFile
+class Config(BaseModel):
+    ...
+    # Add your configuration here
+
+
+# Get your config by using `get_plugin_config(Config)`
+
+CONFIG = get_plugin_config(Config)
